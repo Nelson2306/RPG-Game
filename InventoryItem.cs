153a9b0a781561108fad9b15b7fc1fa2506464de
bence0307 <61406060+bence0307@users.noreply.github.com>
2020-03-03 22:52:39 +0100

    Add files via upload

---
+++ InventoryItem.cs
@@ -0,0 +1,15 @@
+﻿namespace Engine
+{
+    public class InventoryItem
+    {
+
+        public InventoryItem(Item details, int quantity)
+        {
+            Details = details;
+            Quantity = quantity;
+        }
+        public Item Details { get; set; }
+        public int Quantity { get; set; }
+
+    }
+}
