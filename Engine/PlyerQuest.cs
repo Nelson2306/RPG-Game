c4f21d01b462d005de0567bce3dacecee3366a11
bence0307 <61406060+bence0307@users.noreply.github.com>
2020-03-04 08:29:50 +0100

    Add files via upload

---
+++ Engine/PlyerQuest.cs
@@ -0,0 +1,17 @@
+﻿namespace Engine
+{
+
+    public class PlayerQuest
+    {
+
+
+        public PlayerQuest(Quest details)
+        {
+            Details = details;
+            IsCompleted = false;
+        }
+
+        public Quest Details { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
